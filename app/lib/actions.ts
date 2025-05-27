b550dab706385f9bdf022494cc047b6401ce7b4e
Dinh Tran <tvandinh93@gmail.com>
2025-05-27 18:20:57 +0700

    chapter 13 handling errors

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -26,10 +26,14 @@ export async function createInvoice(formData: FormData) {
     });
     const amountInCents = amount * 100;
     const date = new Date().toISOString().split("T")[0];
-    await sql`
+    try {
+        await sql`
     INSERT INTO invoices (customer_id, amount, status, date)
     VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
-  `;
+      `;
+    } catch (e) {
+        console.error(e);
+    }
 
     revalidatePath("/dashboard/invoices");
     redirect("/dashboard/invoices");
@@ -44,11 +48,15 @@ export async function updateInvoice(id: string, formData: FormData) {
 
     const amountInCents = amount * 100;
 
-    await sql`
+    try {
+        await sql`
     UPDATE invoices
     SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
     WHERE id = ${id}
   `;
+    } catch (e) {
+        console.error(e);
+    }
 
     revalidatePath("/dashboard/invoices");
     redirect("/dashboard/invoices");
